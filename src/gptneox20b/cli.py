d3677f91c0b2963cc1853e11094a00d69cd45566
zackees <z@zackees.com>
2024-01-18 17:42:06 -0800

    updates

--- src/gptneox20b/cli.py
+++ src/gptneox20b/cli.py
@@ -6,7 +6,5 @@ import sys
 
 from gptneox20b.run import main
 
-
-
 if __name__ == "__main__":
     sys.exit(main())
--- src/gptneox20b/run.py
+++ src/gptneox20b/run.py
@@ -1,15 +1,25 @@
+# pylint: disable=import-outside-toplevel
+
 # https://colab.research.google.com/drive/1Cngzh5VFrpDqtHcaCYFpW10twsuwGvGy?usp=sharing#scrollTo=D6xY7NlxbIIN
 
-from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast
-import torch
+import argparse
 
-LOW_CPU_MEMORY = True
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--low-cpu-memory", action="store_true")
+    return parser.parse_args()
+
 
 def main() -> int:
+    args = parse_args()
+    import torch
+    from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast
+
     model = GPTNeoXForCausalLM.from_pretrained(
         "EleutherAI/gpt-neox-20b",
         torch_dtype=torch.float32,
-        low_cpu_mem_usage=LOW_CPU_MEMORY
+        low_cpu_mem_usage=args.low_cpu_memory,
     )
     tokenizer = GPTNeoXTokenizerFast.from_pretrained("EleutherAI/gpt-neox-20b")
     input_text = "translate English to German: How old are you?"
@@ -19,11 +29,12 @@ def main() -> int:
         do_sample=True,
         temperature=0.1,
         max_length=200,
-        pad_token_id=tokenizer.eos_token_id
+        pad_token_id=tokenizer.eos_token_id,
     )
     gen_text = tokenizer.batch_decode(gen_tokens)[0]
     print(f"Generated text: {gen_text}")
     return 0
+
 
 if __name__ == "__main__":
     main()
